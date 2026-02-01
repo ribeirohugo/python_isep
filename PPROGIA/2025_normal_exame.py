d3d6f9a3a8c873e37a2137558076cb1183be5cdf
Hugo Ribeiro <ribeirohugo@outlook.pt>
2026-02-01 00:34:50 +0000

    feat: adicionar exercício 2 exame de PPROGIA 2025

--- PPROGIA/2025_normal_exame.py
+++ PPROGIA/2025_normal_exame.py
@@ -3,6 +3,11 @@ def criar_usernames(emails):
         map(lambda e: e.split('@')[0].upper(), emails)
     )
 
+def soma_precos(precos):
+    return sum(
+        map(lambda x: x * 0.85, filter(lambda x: x > 50, precos))
+    )
+
 def main():
     pythonemails = [
         'joao.silva@empresa.pt',
@@ -16,5 +21,10 @@ def main():
     result = criar_usernames(pythonemails)
     print(result)
 
+    precos = [25.50, 80.00, 45.00, 120.00, 35.00, 55.00, 60.00]
+    print('## Exercício 2 ##')
+    result = soma_precos(precos)
+    print(result)
+    
 if __name__ == "__main__":
     main()
